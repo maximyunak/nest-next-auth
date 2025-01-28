63921084fe38343350922e30ed72646e10640880
Maxik-web <kleok0039@gmail.com>
2025-01-28 09:00:57 +0300

    fix redis

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { IS_DEV_ENV } from './libs/common/utils/is-dev.util'
+import { PostController } from './test.controller'
 
 @Module({
 	imports: [
@@ -9,6 +10,7 @@ import { IS_DEV_ENV } from './libs/common/utils/is-dev.util'
 			ignoreEnvFile: !IS_DEV_ENV,
 			isGlobal: true
 		})
-	]
+	],
+	controllers: [PostController]
 })
 export class AppModule {}
